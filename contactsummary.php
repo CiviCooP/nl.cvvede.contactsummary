9bff379b80f3bb70d8145333cc906af42b291c64
Jaap Jansma <jaap.jansma@civicoop.org>
2024-08-29 15:54:28 +0200

    update

--- contactsummary.php
+++ contactsummary.php
@@ -3,79 +3,12 @@
 require_once 'contactsummary.civix.php';
 
 /**
- * Place a link to Kennismakingsgesprek webform
- *
- * Implementation of hook_civicrm_summary
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_summary
- */
-function contactsummary_civicrm_pageRun(&$page) {
-  if ($page instanceof CRM_Contact_Page_View_Summary) {
-    try {
-      $contact = civicrm_api3('Contact', 'getsingle', array(
-        'id' => $page->getVar('_contactId'),
-        'contact_sub_type' => "Buddy",
-      ));
-      $kennismakingsgesprekLink = "https://crm.cvvede.nl/civicrm/contact/kennismakingsgesprek?cid2=".$page->getVar('_contactId');
-      CRM_Core_Region::instance('page-body')->add(array(
-        'template' => "CRM/Contact/Page/View/Summary/link_to_kennismakingsgesprek.tpl"
-      ));
-
-      $smarty = CRM_Core_Smarty::singleton();
-      $smarty->assign('link_to_kennismakingsgesprek', $kennismakingsgesprekLink);
-    } catch (Exception $e) {
-      // Do nothing
-    }
-  }
-}
-
-function contactsummary_civicrm_buildForm($formName, &$form) {
-  if ($formName == 'CRM_Activity_Form_ActivityLinks') {
-    $activityTypeToHide = array();
-    try {
-      $activityTypeToHide[] = civicrm_api3('OptionValue', 'getvalue', array('label' => 'Inschrijving Buddy', 'return' => 'value', 'option_group_id' => 'activity_type'));
-    } catch (Exception $e) {
-      // Do nothing
-    }
-    try {
-      $activityTypeToHide[] = civicrm_api3('OptionValue', 'getvalue', array('label' => 'Oproepje', 'return' => 'value', 'option_group_id' => 'activity_type'));
-    } catch (Exception $e) {
-      // Do nothing
-    }
-    try {
-      $activityTypeToHide[] = civicrm_api3('OptionValue', 'getvalue', array('label' => 'Kennismakingsgesprek', 'return' => 'value', 'option_group_id' => 'activity_type'));
-    } catch (Exception $e) {
-      // Do nothing
-    }
-
-    $activityTypes = $form->get_template_vars('activityTypes');
-    foreach($activityTypes as $activityTypeId => $activityType) {
-      if (in_array($activityTypeId, $activityTypeToHide)) {
-        unset($activityTypes[$activityTypeId]);
-      }
-    }
-    $form->assign('activityTypes', $activityTypes);
-  }
-}
-
-/**
  * Implements hook_civicrm_config().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_config
  */
 function contactsummary_civicrm_config(&$config) {
   _contactsummary_civix_civicrm_config($config);
-}
-
-/**
- * Implements hook_civicrm_xmlMenu().
- *
- * @param array $files
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function contactsummary_civicrm_xmlMenu(&$files) {
-  _contactsummary_civix_civicrm_xmlMenu($files);
 }
 
 /**
@@ -88,96 +21,12 @@ function contactsummary_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_uninstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function contactsummary_civicrm_uninstall() {
-  _contactsummary_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
  */
 function contactsummary_civicrm_enable() {
   _contactsummary_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function contactsummary_civicrm_disable() {
-  _contactsummary_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @param $op string, the type of operation being performed; 'check' or 'enqueue'
- * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
- *
- * @return mixed
- *   Based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
- *                for 'enqueue', returns void
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function contactsummary_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _contactsummary_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function contactsummary_civicrm_managed(&$entities) {
-  _contactsummary_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * @param array $caseTypes
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function contactsummary_civicrm_caseTypes(&$caseTypes) {
-  _contactsummary_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function contactsummary_civicrm_angularModules(&$angularModules) {
-_contactsummary_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function contactsummary_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _contactsummary_civix_civicrm_alterSettingsFolders($metaDataFolders);
 }
 
 /**
@@ -189,9 +38,8 @@ function contactsummary_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_preProcess
  *
-function contactsummary_civicrm_preProcess($formName, &$form) {
 
-} // */
+ // */
 
 /**
  * Implements hook_civicrm_navigationMenu().
